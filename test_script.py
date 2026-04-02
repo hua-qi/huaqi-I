5ace811387d3c573a921c30669720d585cd829a1
lianzimeng05 <lianzimeng05@kuaishou.com>
2026-04-02 14:47:30 +0800

    chore: 清理根目录杂散脚本和残留测试文件
    - 删除根目录 test_json.py test_pty2.py test_script.py test_wrap.py
    - 删除已迁移的 tests/scheduler/test_growth_jobs.py
    - 402 tests pass

--- test_script.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def strip_ansi(text):
-    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-    return ansi_escape.sub('', text)
-
-print(strip_ansi("\x1b[31mHello\x1b[0m World!"))
